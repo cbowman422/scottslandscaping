59dab3c0772fc93ff077b18682c05ebf0cb45fd9
cbowman422 <chrisb0wman@hotmail.com>
2023-03-20 18:50:09 -0400

    routes, pages, components basics

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Main from '../pages/Main';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <h2> hello </h2>
-      </header>
+    
+      <Main />
+    
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <h2> home </h2>
+      <Footer />
+    </div>
+
+  )
+}
+
+export default Home

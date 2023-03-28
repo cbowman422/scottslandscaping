9dd6e40834b561edb21ff63a915e1cc2f27a2190
cbowman422 <chrisb0wman@hotmail.com>
2023-03-28 17:05:36 -0400

    shadow filter for clip path

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,21 +7,23 @@ export const Header = () => {
     const [active, setActive] = useState(false)
 
     return (
-        <div className="header">
-            <div className="header-content">
-                <img className="headerLogo" src={logo} alt="logo" />
-                <div className={active ? 'hamburger active' : 'hamburger'} onClick={()=>setActive(!active)}>
-                    <span>&nbsp;</span>
-                    <span>&nbsp;</span>
-                    <span>&nbsp;</span>
+        <div className="header-shadow">
+            <div className="header">
+                <div className="header-content">
+                    <img className="headerLogo" src={logo} alt="logo" />
+                    <div className={active ? 'hamburger active' : 'hamburger'} onClick={()=>setActive(!active)}>
+                        <span>&nbsp;</span>
+                        <span>&nbsp;</span>
+                        <span>&nbsp;</span>
+                    </div>
                 </div>
-            </div>
-            <div className={active ? 'menu active' : 'menu'}>
-                <div className="nav-item"><a className="navListA" href="/" >HOME</a></div>
-                <div className="nav-item"><a className="navListA" href="/portfolio" >PORTFOLIO</a></div>
-                <div className="nav-item"><a className="navListA" href="/about" >ABOUT&nbsp;US</a></div>
-                <div className="nav-item"><a className="navListA" href="/services" >SERVICES</a></div>
-                <div className="nav-item"><a className="navListA" href="/contact" >CONTACT</a></div>
+                <div className={active ? 'menu active' : 'menu'}>
+                    <div className="nav-item"><a className="navListA" href="/" >HOME</a></div>
+                    <div className="nav-item"><a className="navListA" href="/portfolio" >PORTFOLIO</a></div>
+                    <div className="nav-item"><a className="navListA" href="/about" >ABOUT&nbsp;US</a></div>
+                    <div className="nav-item"><a className="navListA" href="/services" >SERVICES</a></div>
+                    <div className="nav-item"><a className="navListA" href="/contact" >CONTACT</a></div>
+                </div>
             </div>
         </div>
 

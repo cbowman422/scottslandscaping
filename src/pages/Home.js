fa66147097d9c3922ee779f537c3c711b06f7aa9
cbowman422 <chrisb0wman@hotmail.com>
2023-03-28 16:05:46 -0400

    hero css styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,10 +5,19 @@ import image1 from '../images/13-164-068-walls.jpeg'
 const Home = () => {
   return (
     <div className="homeContainer">
-       <img className="heroHome" src={image1} alt="stone wall"/>
+
+      <div className="heroContainer" > 
+         <h1> Scott's Landscaping, Inc. <span className=""> Hopkinton, MA </span></h1> 
+         <h2> Scott's Landscaping is a full service lawn care, landscape management, and design company specializing in all areas of stonework.</h2>
+      </div>
+       
     </div>
 
   )
 }
 
 export default Home;
+
+
+
+{/* <img className="heroHome" src={image1} alt="stone wall"/> */}

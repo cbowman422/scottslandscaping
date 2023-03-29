e82019d5567fb5787087d9f7a927609c8210a9b9
cbowman422 <chrisb0wman@hotmail.com>
2023-03-29 12:54:02 -0400

    home text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,17 +15,22 @@ const Home = () => {
       
       <div className="hero-container" > 
          <h1> Scott's Landscaping, Inc. <span className=""> Hopkinton, MA </span></h1> 
-         <h2> Scott's Landscaping is a full service lawn care, landscape management, and design company specializing in all areas of stonework.</h2>
+         <h2> Scott's Landscaping is a full service lawn care, landscape management and design company specializing in all areas of stonework.</h2>
          <a href="/" > Schedule a consultation </a>
       </div>
        
       <div className="home-welcome-statement-container">
 
         <h3> Welcome ! </h3>
-        <p> Scott's offers service in New England to residential customers, contractors, commercial and industrial facilities. </p>
-        <p> We offer our customers the advantage of one company to handle all of their landscaping needs.
-        Contact us for your free, no-obligation estimate of services that best fits your needs as well
-        as your budget.
+
+        <p>
+          At Scott's Landscaping, Inc. We offer service in New England to residential customers, contractors, commercial and industrial facilities. 
+
+          We provide our customers the advantage of having one company to handle all of their landscaping needs.
+          
+          Scott's team of experienced professionals is well-versed in landscape design, landscape architecture, custom stone work, stone walls, architectural pool landscapes, walkways, driveways, backyard elements, landscape lighting, and garden design.
+
+          Whether you're looking to enhance your home's curb appeal or create a peaceful sancutary in your backyard, Scott's will transform your outdoor space into a beautiful and inviting extension of your home.
         </p>
 
 
@@ -33,20 +38,35 @@ const Home = () => {
           <h3> What you can expect! </h3>
           <div className="home-welcome-details">
             <img src={expertise} alt="expertise"/>
-            <h3>Expertise</h3>
+            <h3>
+              Expertise
+            </h3>
             <p>
-            With over 25 years of experience our team of experts can transform any outdoor area into a stunning oasis. From creating custom stonework to landscape design and maintenance we have the skill and knowledge to make your landscaping dreams a reality.
+              With over 25 years of experience Scott's team of professionals can transform any outdoor area into a stunning oasis. From custom stonework to landscape design and maintenance we have the skills and knowledge to make your landscaping dreams a reality.
             </p>
+            <a>Learn more about the team</a>
           </div>
+
           <div className="home-welcome-details">
             <img src={flexibility} alt="flexibility"/>
-            <h3>Flexibility</h3>
-            <p>At Scott's we understand that each client and property is unique, and we take pride in our ability to adapt and customize our services to fit each individual project. </p>
+            <h3>
+              Flexibility
+            </h3>
+            <p>
+              At Scott's we understand that each client and property is unique, and we take pride in our ability to adapt and customize our services to fit each individual project. 
+            </p>
+            <a>Schedule a consultation</a>
           </div>
+
           <div className="home-welcome-details">
             <img src={attentionDetail} alt="attention to detail"/>
-            <h3>Attention to Detail</h3>
-            <p> From the initial consultation to the final touches, we are dedicated to ensuring that every detail is accounted for and executed flawlessly. </p>
+            <h3>
+              Attention to Detail
+            </h3>
+            <p> 
+              From the initial consultation to the final touches, we are dedicated to ensuring that every detail is accounted for and executed flawlessly. 
+            </p>
+            <a>Go to the Portfolio.</a>
           </div>
 
         </div>

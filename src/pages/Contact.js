54b884efac29ca4c9c25b2e7aac9de9eb731a529
cbowman422 <chrisb0wman@hotmail.com>
2023-04-15 15:26:15 -0400

    services mobile

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+
 const Contact = () => {
   return (
     <div className="contact-container">
       <h3> Contact </h3>
+
+      {/* <a href={newDoc} download> document </a> */}
     </div>
   )
 }

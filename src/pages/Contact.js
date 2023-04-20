de094589729309ea4b5328f04aba1466bffd1144
cbowman422 <chrisb0wman@hotmail.com>
2023-04-20 14:41:05 -0400

    color change

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Contact = () => {
   return (
     <div className="contact-container">
-      <h3> Contact </h3>
+      <h3> Contact add where do they live</h3>
 
       {/* <a href={newDoc} download> document </a> */}
     </div>

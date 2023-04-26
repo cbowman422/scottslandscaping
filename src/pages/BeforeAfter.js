96614d423eb588a13336efdfea916a1e970c7fb2
cbowman422 <chrisb0wman@hotmail.com>
2023-04-26 16:32:22 -0400

    before and after photos and repairs

--- src/pages/BeforeAfter.js
+++ src/pages/BeforeAfter.js
@@ -2,8 +2,17 @@ import React from 'react';
 import BeforeAfterSlider from '../components/BeforeAfterSlider';
 import '../css/BeforeAfter.css'
 
-import image1 from '../images/13-142-151-pool.jpeg';
-import image2 from '../images/13-142-209-walls.jpeg'
+import ba1b from '../images/BA1B.jpeg';
+import ba1a from '../images/BA1A.jpeg';
+
+import ba2b from '../images/BA2B.jpeg';
+import ba2a from '../images/BA2A.jpeg';
+
+
+import ba3b from '../images/BA3B.jpeg';
+import ba3a from '../images/BA3A.jpeg';
+
+
 
 const BeforeAfter = () => {
   return (
@@ -14,8 +23,12 @@ const BeforeAfter = () => {
       <h3 className="BeforeAfter-h3" > Before & After </h3>
 
       <div className="beforeAfter-container-flex" >
-        <BeforeAfterSlider before={image1} after={image2}/>
-        <BeforeAfterSlider before={image1} after={image2}/>
+        <BeforeAfterSlider before={ba1b} after={ba1a}/>
+        <BeforeAfterSlider before={ba2b} after={ba2a}/>
+        </div>
+        <div className="beforeAfter-container-flex" >
+        <BeforeAfterSlider before={ba3b} after={ba3a}/>
+        <BeforeAfterSlider before={ba3b} after={ba3a}/>
       </div>
     </div>
   )

3d4dd05641d71795998d095805db31055f63f8e4
cbowman422 <chrisb0wman@hotmail.com>
2023-04-26 16:55:07 -0400

    before and after sliders

--- src/pages/BeforeAfter.js
+++ src/pages/BeforeAfter.js
@@ -12,6 +12,15 @@ import ba2a from '../images/BA2A.jpeg';
 import ba3b from '../images/BA3B.jpeg';
 import ba3a from '../images/BA3A.jpeg';
 
+import ba4b from '../images/BA4B.jpeg';
+import ba4a from '../images/BA4A.jpeg';
+
+import ba5b from '../images/BA5B.jpeg';
+import ba5a from '../images/BA5A.jpeg';
+
+import ba6b from '../images/BA6B.jpeg';
+import ba6a from '../images/BA6A.jpeg';
+
 
 
 const BeforeAfter = () => {
@@ -28,7 +37,11 @@ const BeforeAfter = () => {
         </div>
         <div className="beforeAfter-container-flex" >
         <BeforeAfterSlider before={ba3b} after={ba3a}/>
-        <BeforeAfterSlider before={ba3b} after={ba3a}/>
+        <BeforeAfterSlider before={ba4b} after={ba4a}/>
+      </div>
+      <div className="beforeAfter-container-flex" >
+        <BeforeAfterSlider before={ba5b} after={ba5a}/>
+        <BeforeAfterSlider before={ba6b} after={ba6a}/>
       </div>
     </div>
   )

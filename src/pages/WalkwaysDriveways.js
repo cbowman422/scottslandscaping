44fe55f953195029d90e7e9331bd175b9b2f23d5
cbowman422 <chrisb0wman@hotmail.com>
2023-04-28 11:03:45 -0400

    walks mobile

--- src/pages/WalkwaysDriveways.js
+++ src/pages/WalkwaysDriveways.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import '../css/WalkwaysDriveways.css';
 
 const WalkwaysDriveways = () => {
   return (
-    <div>WalkwaysDriveways</div>
+    <div className="walks-gallery-container">
+
+    <a className="return-before" href="/gallery" > &#60; Return to Gallery </a>
+    
+          <h3> Walkways & Driveways </h3>
+          <div className="walks-gallery-item-container" >
+            <div className="walks-gallery-item" id="walks-gallery-item-1"></div>
+            <div className="walks-gallery-item" id="walks-gallery-item-2"></div>
+            <div className="walks-gallery-item" id="walks-gallery-item-3"></div>
+            <div className="walks-gallery-item" id="walks-gallery-item-4"></div>
+            <div className="walks-gallery-item" id="walks-gallery-item-5"></div>
+            <div className="walks-gallery-item" id="walks-gallery-item-6"></div>
+            <div className="walks-gallery-item" id="walks-gallery-item-7"></div>
+            <div className="walks-gallery-item" id="walks-gallery-item-8"></div>
+            <div className="walks-gallery-item" id="walks-gallery-item-9"></div>
+            <div className="walks-gallery-item" id="walks-gallery-item-10"></div>
+            <div className="walks-gallery-item" id="walks-gallery-item-11"></div>
+            <div className="walks-gallery-item" id="walks-gallery-item-12"></div>
+          </div>
+        </div>
   )
 }
 

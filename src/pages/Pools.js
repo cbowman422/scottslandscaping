7d703e17d49afabb38958f307ebf38d23bfa0cf8
cbowman422 <chrisb0wman@hotmail.com>
2023-04-28 11:35:02 -0400

    pools mobile

--- src/pages/Pools.js
+++ src/pages/Pools.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import '../css/Pools.css';
 
 const Pools = () => {
   return (
-    <div>Pools</div>
+    <div className="pools-gallery-container">
+
+    <a className="return-before" href="/gallery" > &#60; Return to Gallery </a>
+    
+          <h3> Pool Landscapes </h3>
+          <div className="pools-gallery-item-container" >
+            <div className="pools-gallery-item" id="pools-gallery-item-1"></div>
+            <div className="pools-gallery-item" id="pools-gallery-item-2"></div>
+            <div className="pools-gallery-item" id="pools-gallery-item-3"></div>
+            <div className="pools-gallery-item" id="pools-gallery-item-4"></div>
+            <div className="pools-gallery-item" id="pools-gallery-item-5"></div>
+            <div className="pools-gallery-item" id="pools-gallery-item-6"></div>
+            <div className="pools-gallery-item" id="pools-gallery-item-7"></div>
+            <div className="pools-gallery-item" id="pools-gallery-item-8"></div>
+            <div className="pools-gallery-item" id="pools-gallery-item-9"></div>
+            <div className="pools-gallery-item" id="pools-gallery-item-10"></div>
+            <div className="pools-gallery-item" id="pools-gallery-item-11"></div>
+            <div className="pools-gallery-item" id="pools-gallery-item-12"></div>
+          </div>
+        </div>
   )
 }
 

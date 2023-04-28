015002d883b8e4f4371ad540ac98c0e15cc98445
cbowman422 <chrisb0wman@hotmail.com>
2023-04-28 12:09:07 -0400

    backyard mobile

--- src/pages/BackyardElements.js
+++ src/pages/BackyardElements.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import '../css/BackyardElements.css';
 
 const BackyardElements = () => {
   return (
-    <div>BackyardElements</div>
+    <div className="backyard-gallery-container">
+
+    <a className="return-before" href="/gallery" > &#60; Return to Gallery </a>
+    
+          <h3> Backyard Elements </h3>
+          <div className="backyard-gallery-item-container" >
+            <div className="backyard-gallery-item" id="backyard-gallery-item-1"></div>
+            <div className="backyard-gallery-item" id="backyard-gallery-item-2"></div>
+            <div className="backyard-gallery-item" id="backyard-gallery-item-3"></div>
+            <div className="backyard-gallery-item" id="backyard-gallery-item-4"></div>
+            <div className="backyard-gallery-item" id="backyard-gallery-item-5"></div>
+            <div className="backyard-gallery-item" id="backyard-gallery-item-6"></div>
+            <div className="backyard-gallery-item" id="backyard-gallery-item-7"></div>
+            <div className="backyard-gallery-item" id="backyard-gallery-item-8"></div>
+            <div className="backyard-gallery-item" id="backyard-gallery-item-9"></div>
+          </div>
+        </div>
   )
 }
 

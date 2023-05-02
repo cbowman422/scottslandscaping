f0726d7ef7db4ad9fe25cefe12303c3ff5a8144b
cbowman422 <chrisb0wman@hotmail.com>
2023-05-02 17:46:47 -0400

    plant finder link in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,7 @@ const Footer = () => {
     <div className="footerContainer">
       <h3> SCOTT'S LANDSCAPING, INC © 2023 </h3>
       <a href="tel:508-435-3911">508-435-3911</a>
+      <a href = "https://www.westonnurseries.com/plant-library/" target="blank">Plant Finder</a>
     </div>
   )
 }
